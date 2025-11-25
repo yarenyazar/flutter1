c5d11795c4e52e4a95ad3baf65596eaa675ec783
yaren-yazar <yarenyazar67@gmail.com>
2025-11-25 23:39:01 +0300

    modified

--- flutter1/lib/degisken_olusturma.dart
+++ flutter1/lib/degisken_olusturma.dart
@@ -8,4 +8,16 @@ void main() {
   print(ogrenciYasi);
   print(ogrenciBoyu);
   print(ogrenciBasHarfi);
+
+  int urun_id = 3416;
+  String urun_adi = "Kol saati";
+  int urun_adet = 100;
+  double urun_fiyat = 149.99;
+  String urun_tedarikci = "rolex";
+
+  print("Ürün id: $urun_id");
+  print("Ürün adı: $urun_adi");
+  print("Ürün adet: $urun_adet");
+  print("Ürün fiyat: $urun_fiyat");
+  print("Ürün tedarikçi: $urun_tedarikci");
 }
